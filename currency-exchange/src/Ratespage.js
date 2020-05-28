2ae4185438ee0ded19c58adb8c4022ec6c3c27dd
Harievashini <harievashini.dk@outlook.com>
2020-05-28 09:29:27 -0700

    modified

--- currency-exchange/src/Ratespage.js
+++ currency-exchange/src/Ratespage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Table} from "reactstrap";
 import axios from "axios";
+
 const tableStyle={
     color:'white',
   };
+
 class Ratespage extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -49,11 +52,6 @@ class Ratespage extends React.Component {
       });
   }
 
-  selectHandler = (event) => {
-    if (event.target.name === "from") {
-      this.setState({ fromCurrency: event.target.value });
-    } 
-  };
   convertHandler = () =>axios
       .get(`https://api.exchangeratesapi.io/latest?base=${this.state.fromCurrency}`)
       .then((response) => {
@@ -74,8 +72,7 @@ class Ratespage extends React.Component {
             })
             .catch((err) => {
               console.log("oops", err);
-            });   
-          
+            });     
         }
         this.setState({ currencies: currencycountry, currencyrates:currencyrates});
       })
@@ -93,54 +90,54 @@ class Ratespage extends React.Component {
     		</div>
      		<div className="row">
      			<div className="col-lg-6 col-xl-6">
-		    	  	<Table borderless >
-		  			  	<thead>
-		    			  	<tr>
-		      					<th style={tableStyle}>{this.state.fromCurrency}</th>
-		      					<th style={tableStyle}>1.00 {this.state.fromCurrency}</th>
-		      					<th style={tableStyle}>inv. 1.00 {this.state.fromCurrency}</th>
-		    			  	</tr>
-		  			  	</thead>
-		  			  	<tbody>
-		    			  	<tr>
-		      					<td style={tableStyle}>{this.state.currencies.map((cur) => (
-		                        	<tr>{cur}</tr>
-		                      	))}</td>
-		      					<td style={tableStyle}>{this.state.currencyrates.map((cur) => (
-		                        	<tr>{cur}</tr>
-		                      	))}</td>
-		      					<td style={tableStyle}>{this.state.invcurrencies.map((cur) => (
-		                        	<tr>{cur}</tr>
-		                      	))}</td>
-		    			  	</tr>
-		    		  	</tbody>
+		    	  <Table borderless >
+		  			  <thead>
+		    			  <tr>
+		      				<th style={tableStyle}>{this.state.fromCurrency}</th>
+		      				<th style={tableStyle}>1.00 {this.state.fromCurrency}</th>
+		      				<th style={tableStyle}>inv. 1.00 {this.state.fromCurrency}</th>
+		    			  </tr>
+		  			  </thead>
+		  			  <tbody>
+		    			  <tr>
+		      				<td style={tableStyle}>{this.state.currencies.map((cur) => (
+		                        <tr>{cur}</tr>
+		                      ))}</td>
+		      				<td style={tableStyle}>{this.state.currencyrates.map((cur) => (
+		                        <tr>{cur}</tr>
+		                      ))}</td>
+		      				<td style={tableStyle}>{this.state.invcurrencies.map((cur) => (
+		                        <tr>{cur}</tr>
+		                      ))}</td>
+		    			  </tr>
+		    		  </tbody>
 				    </Table>
-		      	</div>
-		      	<div className="col-lg-6 col-xl-6">
-		      		<div className="row">
-		      			<div className="col-lg-12 col-xl-12">
-		      				<br></br>
-                  <center>
-                  <h5>Choose the currency</h5>
-                  <select
-                    name="from"
-                    onChange={(event) => this.selectHandler(event)}
-                    value={this.state.fromCurrency}
-                  >
-                    {this.state.currency.map((cur) => (
+		      </div>
+		      <div className="col-lg-6 col-xl-6">
+		      	<div className="row">
+		      		<div className="col-lg-12 col-xl-12">
+		      			<br></br>
+                <center>
+                <h5>Choose the currency</h5>
+                <select
+                  name="from"
+                  onChange={(event) => this.selectHandler(event)}
+                  value={this.state.fromCurrency}
+                >
+                  {this.state.currency.map((cur) => (
                     <option key={cur}>{cur}</option>
-                    ))}
-                  </select>
-                  <br></br>
-                  <br></br>
-                  <button onClick={this.convertHandler}>Go</button>
-                  <br></br>
-                  </center>
-		      			</div>
+                  ))}
+                </select>
+                <br></br>
+                <br></br>
+                <button onClick={this.convertHandler}>Go</button>
+                <br></br>
+                </center>
 		      		</div>
 		      	</div>
+		      </div>
 		    </div>
-		</div>
+		  </div>
     );
   }
 }
