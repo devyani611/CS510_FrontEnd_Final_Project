2451472ff3580b8cb71e8ba78399ebf22c506961
Harievashini <harievashini.dk@outlook.com>
2020-05-28 09:21:29 -0700

    modified

--- currency-exchange/src/App.js
+++ currency-exchange/src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div className="row">
       <Router>
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-          <Navigation/>
+          <Navigation />
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/Ratespage" component={Ratespage} />
