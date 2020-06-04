0e8959a09d724d3a131df3b537b0ef165dd6e670
devyani611 <devyani@pdx.edu>
2020-06-04 15:54:59 -0700

    Updates

---
+++ precache-manifest.c124af05f44f11a3b19e400953aff4ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c682c1dca4ebabf33f84790fed6830",
+    "url": "/CS510_FrontEnd_Final_Project/index.html"
+  },
+  {
+    "revision": "440c2b877245d5b615ec",
+    "url": "/CS510_FrontEnd_Final_Project/static/css/2.80468ea7.chunk.css"
+  },
+  {
+    "revision": "e08241f09bc5c29a0344",
+    "url": "/CS510_FrontEnd_Final_Project/static/css/main.6b830825.chunk.css"
+  },
+  {
+    "revision": "440c2b877245d5b615ec",
+    "url": "/CS510_FrontEnd_Final_Project/static/js/2.dfdbbcb6.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/CS510_FrontEnd_Final_Project/static/js/2.dfdbbcb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08241f09bc5c29a0344",
+    "url": "/CS510_FrontEnd_Final_Project/static/js/main.ae63d5d5.chunk.js"
+  },
+  {
+    "revision": "09ba3c57f8bb5772c3e1",
+    "url": "/CS510_FrontEnd_Final_Project/static/js/runtime-main.9467be4b.js"
+  }
+]);
