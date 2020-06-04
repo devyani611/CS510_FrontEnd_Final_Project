85f6151bd57a9922bd8497ed7c5f2601fe445bae
devyani611 <devyani@pdx.edu>
2020-06-04 16:48:09 -0700

    Updates

---
+++ precache-manifest.4e74b1d455fece7fa79b69b82bb003de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fd70297f59a232a8443b16ec421b6e",
+    "url": "/CS510_FrontEnd_Final_Project/currency-exchange/index.html"
+  },
+  {
+    "revision": "880cf70d41488b26d628",
+    "url": "/CS510_FrontEnd_Final_Project/currency-exchange/static/css/2.80468ea7.chunk.css"
+  },
+  {
+    "revision": "3be72a934ec91670c9bc",
+    "url": "/CS510_FrontEnd_Final_Project/currency-exchange/static/css/main.af4d9bbb.chunk.css"
+  },
+  {
+    "revision": "880cf70d41488b26d628",
+    "url": "/CS510_FrontEnd_Final_Project/currency-exchange/static/js/2.9ff1fad1.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/CS510_FrontEnd_Final_Project/currency-exchange/static/js/2.9ff1fad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be72a934ec91670c9bc",
+    "url": "/CS510_FrontEnd_Final_Project/currency-exchange/static/js/main.bc5ac6aa.chunk.js"
+  },
+  {
+    "revision": "07b625bc156126778761",
+    "url": "/CS510_FrontEnd_Final_Project/currency-exchange/static/js/runtime-main.21af4b1e.js"
+  }
+]);
